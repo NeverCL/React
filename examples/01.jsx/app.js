40e56967bab02ccb0ef8cbc2cb10bf56bd521c31
Never、C <caolin>
2016-11-29 17:51:26 +0800

    add render

--- examples/01.jsx/app.js
+++ examples/01.jsx/app.js
@@ -1,7 +1,18 @@
+ReactDOM.render(React.createElement('h1', null, 'Hello, world!'), document.getElementById('root'))
+
+ReactDOM.render(<h1>Hello1</h1>, document.getElementById('root1'))
+
 var Hello = React.createClass({
-    render: function () {
-        return <h1>Hello World!</h1>
+    render: function() {
+        return <h1>Hello World2!</h1>
     }
 });
+ReactDOM.render(<Hello />, document.getElementById('root2'))
 
-ReactDOM.render(<Hello />, document.getElementById('root'))
+
+var Hello = React.createClass({
+    render: function() {
+        return <h1>Hello {this.props.title}3!</h1>
+    }
+});
+ReactDOM.render(<Hello title="World"/>, document.getElementById('root3'))
